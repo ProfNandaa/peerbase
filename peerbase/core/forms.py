36f41df282223436838870fa60e733c3c1552075
linux <mwalughabura@gmail.com>
2016-05-22 18:45:47 +0300

    The form is getting the data but have does not redirect to the next page.

--- peerbase/core/forms.py
+++ peerbase/core/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 
-class userForm(forms.Form):
-	full_name = forms.CharField(label='first name', min_length=3, max_length=100)
+class registerForm(forms.Form):
+	fullname = forms.CharField(label='Full name', min_length=3, max_length=100)
 	email = forms.CharField(label='email', max_length=100, min_length=3)
 	password = forms.CharField(label='email', min_length=6, max_length=50)
 
 
 def save(self, data):
-	u = User.objects.create_user(data['full_name'],
+	u = User.objects.create_user(data['name'],
 								data['email'],
 								data['password'])
 	return u
--- peerbase/core/views.py
+++ peerbase/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 
-from forms import userForm
+from .forms import RegisterForm
 
 # Create your views here.
 
@@ -10,22 +10,24 @@ def index(request):
 	return render(request, 'core/index.html')
 
 def register(request):
-	if request.user.is_authenticated():
-		return redirect(home)
 	if request.method == 'POST':
-		import pdb; pdb.set_trace();
-		form = UserForm(request.POST)
+		form = RegisterForm(request.POST)
+		data = {}
+		fullname = form.cleaned_data['name'].split()
+		data['first_name'] = fullname[0]
+		data['last_name'] = fullname[1]
+		data['email']=form.cleaned_data['email']
+		data['password']=form.cleaned_data['password']
+		form = registerForm(request.POST)
+
 		if form.is_valid():
-			data = {}
-			fullname = form.cleaned_data['name'].split()
-			data['first_name'] = fullname[0]
-			data['last_name'] = fullname[1]
-			data['email']=form.cleaned_data['email']
-			data['password']=form.cleaned_data['password']
+			return HttpResponseRedirect('/Thank you!/')
+	else:
+		form = registerForm()
 
-			form.save(data)# Saves the user
+		form.save(data)
+		request.session['registered']=True
 
-			request.session['registered']=True # For display purpose
 	return render(request, 'core/register.html')
 
 def login(request):
